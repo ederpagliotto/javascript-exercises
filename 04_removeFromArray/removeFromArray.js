1604f6ffe3a0c89fe2be6ecac1518def70841fad
Eder Pagliotto <ederpagliotto@gmail.com>
2022-03-02 19:43:06 +0000

    4th exercise complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,9 @@
-const removeFromArray = function() {
-
-};
-
+let array = [1, 2, 3, 4];               
+let values = [1, 3];
+const removeFromArray = function(array) {                      
+array = array.filter(item => (!values.includes(item)));  //array recebe filtro com item tal que o valor ! nao seja incluso.
+return array;                                            // por isso o uso da negacao "!"
+};                                                       // resulta o array filtrado SEM incluir os valores.
+removeFromArray(array);
 // Do not edit below this line
 module.exports = removeFromArray;
