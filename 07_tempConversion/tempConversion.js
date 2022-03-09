24db060fc97c0a9808ad70a26e148acd46386db6
Eder Pagliotto <ederpagliotto@gmail.com>
2022-03-09 17:02:26 +0000

    exercise 07 complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
-
+let fahrenheit;
+let celsius;
+let conversion;
+                         //Fahrenheit to Celsius
+const ftoc = function(fahrenheit = -100) {             //value to convert
+  conversion = (fahrenheit - 32) * 5/9;                //formula
+  return parseFloat(conversion.toFixed(1));           // tofixed returns string, so I need to do a ParseFloat.
 };
-
-const ctof = function() {
-
+                         // Celsius to Fahrenheit
+const ctof = function(celsius = -10) {                //value to convert
+  conversion = (celsius * 9/5) + 32;                    // formula
+  return parseFloat(conversion.toFixed(1));            // tofixed(1) returns 1 number after pontuation.
 };
 
 // Do not edit below this line
