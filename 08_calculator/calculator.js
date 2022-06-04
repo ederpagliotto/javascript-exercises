3f75cf8db6764929be071107f2af6750e2ec3216
Eder Pagliotto <ederpagliotto@gmail.com>
2022-06-04 10:00:40 +0100

    calculator complete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+let a = 4;
+let b = 3;
+let n = 10;
+
+const add = function(a,b) {
+return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+	return a.reduce((total, current) => total + current, 0);
 };
 
 const multiply = function() {
-
+  return b.length 
+  ? b.reduce((total, nextNumber) => total * nextNumber) : 0;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if (n === 0) return 1;
+  let product = 1;
+  for (let i = n; i > 0; i--) {
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
