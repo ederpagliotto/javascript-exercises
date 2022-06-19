3d60664d6a4b68d7a0c14c887160bd7bf964a2a0
Eder Pagliotto <ederpagliotto@gmail.com>
2022-06-19 10:28:19 +0100

    fibonacci complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(count) {
+    if (count < 0) return "OOPS";
+    if (count === 0) return 0;
 
+    let a = 0;
+    let b = 1;
+
+    for (let i = 1; i < count; i++) {
+    let temp = b;
+    
+    b = a + b;
+    a = temp;
+    }
+    return b;
 };
+
+let count = "8";
 
 // Do not edit below this line
 module.exports = fibonacci;
